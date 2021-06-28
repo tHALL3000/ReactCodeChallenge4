edfdb50a350b95c108e8a6e9bd1eeb64efb6863c
tHALL3000 <80265634+tHALL3000@users.noreply.github.com>
2021-06-27 18:41:06 -0600

    useState and if/then

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ScotchInfoBar from './ScotchInfoBar';
-import './styles.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import ScotchInfoBar from "./ScotchInfoBar";
+import "./styles.css";
 
 function App() {
+  const [phrase, setPhrase] = useState("");
+  if (phrase === "open sesame") {
+    alert("You may pass!");
+  }
+
   return (
     <div className="App">
       <h2>What's the secret phrase?</h2>
 
-      <input type="text" placeholder="Super duper secret" />
-
+      <input
+        type="text"
+        placeholder="Super duper secret"
+        value={phrase}
+        onChange={(e) => setPhrase(e.target.value)}
+      />
       <p>
         Hint: It's <strong>open sesame</strong>
       </p>
@@ -19,5 +28,5 @@ function App() {
   );
 }
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
